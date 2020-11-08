69d3b138f17f214ae86dbaeeaf398da06b73acbf
Egor <egorsilin@gmail.com>
2020-11-08 14:16:42 +0500

    added samples files

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import os
 print('Hello!')
+print('Hello!')
+print(os.path.dirname(os.path.abspath(__file__)))
+
+
+if __name__ == '__main__':
+    timeout = 1  # время в минутах
+    val1 = ''  # путь к папке с файлами (формат файлов либо текстовый либо архивный)
+    val2 = ''  # путь к папке с конфигами (файлы с расширением .cfg)
+
+
+
