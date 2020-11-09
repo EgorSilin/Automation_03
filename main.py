3d4d71125d568c3b31afbbee36e45578adf8e199
Egor <egorsilin@gmail.com>
2020-11-09 15:41:35 +0500

    added read/write log in csv

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 import datetime
 import binascii
+import csv
 
 
 def take_cfg_dict(val2_inp):
@@ -19,8 +20,8 @@ def take_cfg_dict(val2_inp):
             continue
         with open(f'{val2_inp}{config_file}', 'r') as f:
             data_file_name = (f.readline()).rstrip('\n').lstrip('name:')
-            print(data_file_name)
-            print(f'XXX: {val2_inp}{config_file}')
+            # print(data_file_name)
+            # print(f'XXX: {val2_inp}{config_file}')
             if data_file_name in cfg_dict_in_f:
                 cfg_dict_in_f[data_file_name].append(f'{val2_inp}{config_file}')
             else:
@@ -31,7 +32,7 @@ def take_cfg_dict(val2_inp):
 
 def check_1st_cond(val1_inp, val2_inp, data_file_path_inp):
     """Func checking 1st condition"""
-    # print(f'Check file in default.cfg')  # comment for prod
+    print(f'Check 1st condition.')  # comment for prod
     file_in_def_cfg_key = False
     with open(f'{val2_inp}default.cfg', 'r') as f:
         for line in f.readlines():
@@ -57,7 +58,7 @@ def check_1st_cond(val1_inp, val2_inp, data_file_path_inp):
 
 def check_2nd_cond(cfg_dict_inp, data_file_inp):
     """Func checking 2nd condition"""
-    # print(f'Check 2nd condition!')
+    print(f'Check 2nd condition!')
     if data_file_inp in cfg_dict_inp:
         for cfg_path in cfg_dict_inp[data_file_inp]:
             print(f'Read config: {cfg_path}')  # comment for prod
@@ -68,19 +69,17 @@ def check_2nd_cond(cfg_dict_inp, data_file_inp):
                     stripped_cfg_line = cfg_line.rstrip('\n')
                     sig_list.append(stripped_cfg_line)
                     # print(stripped_cfg_line)
-                print(sig_list)
+                print(f"SIG 1xLIST: {sig_list}")
                 sig_list_2 = [[sig_list[i], sig_list[i + 1]] for i in range(0, len(sig_list) - 1, 2)]
-                print(sig_list_2)
+                print(f"SIG 2xLIST: {sig_list_2}")
                 for sig in sig_list_2:
-                    print(cfg_path)
-                    print(sig[0])
-                    print(sig[1])  # should be hex str
+                    print(f"SIG '{sig[1]}' should be in {sig[0]} minutes")
                     '''XXX = 585858, YYY = 595959, ZZZ = 5a5a5a'''
                     # hex_sig = binascii.hexlify(sig[1].encode('utf8'))  # from str to hex str
                     # print(hex_sig)
                     # unhex_sig = binascii.unhexlify(hex_sig).decode('utf8')  # from hex str to str
                     # print(unhex_sig)
-                    print(data_file_path)
+                    print(f"FILE PATH: {data_file_path}")
                     with open(data_file_path, 'rb') as ff:
                         file_bytes = ff.read()
                         file_hex = file_bytes.hex()
@@ -88,8 +87,24 @@ def check_2nd_cond(cfg_dict_inp, data_file_inp):
                         print(f'FILE IN HEX: {file_hex}, type: {type(file_hex)}')  # comment for prod
                         if sig[1] in file_hex:
                             print(f"Alarm! Signature '{sig[1]} detected in {data_file_path}!'")  # comment for prod
+                            # write log in csv
+                            with open("sig_det_log.csv", mode="a", encoding='utf-8') as w_file:
+                                file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
+                                file_writer.writerow([datetime.datetime.now(),
+                                                      sig[1],
+                                                      data_file_path])
                         else:
                             print(f"Ok! Signature '{sig[1]}' was not detected in file '{data_file_path}'")
+                            # check csv log file
+                            with open("sig_det_log.csv", mode='r', encoding='utf-8') as r_file:
+                                file_reader = csv.reader(r_file, delimiter=";")
+                                count = 0
+                                for row in file_reader:
+                                    print(row[0], row[1], row[2])
+                                print(f'Всего в файле {count} строк.')
+                            # END check csv file
+    else:
+        print(f'File is not in config files.')
 
 
 if __name__ == '__main__':
@@ -97,18 +112,17 @@ if __name__ == '__main__':
     timeout = 100
     timedelta_timeout = datetime.timedelta(minutes=timeout)
     # путь к папке с файлами (формат файлов либо текстовый либо архивный)
-    # val1 = '/home/user/PythonProjects/Automation_03/val1_data_files/'
-    val1 = '/home/user/PycharmProjects/Automation_03/val1_data_files/'
+    val1 = f'{os.path.dirname(os.path.abspath(__file__))}/val1_data_files/'
     # путь к папке с конфигами (файлы с расширением .cfg)
-    # val2 = '/home/user/PythonProjects/Automation_03/val2_config_files/'
-    val2 = '/home/user/PycharmProjects/Automation_03/val2_config_files/'
+    val2 = f'{os.path.dirname(os.path.abspath(__file__))}/val2_config_files/'
 
     data_files_lst = sorted(os.listdir(val1))
     config_files_lst = sorted(os.listdir(val2))
     cfg_dict = take_cfg_dict(val2)
-    print(cfg_dict)
-    print(data_files_lst)
-    print(config_files_lst)
+    print('\n' + ("=" * 100) + '\n')
+    print(f'File in config dict: {cfg_dict}')
+    print(f'Data files list: {data_files_lst}')
+    print(f'Config file list: {config_files_lst}')
     for data_file in data_files_lst:
         data_file_path = f"{val1}{data_file}"
         print(f'\nCheck file: {data_file_path}')
@@ -116,6 +130,6 @@ if __name__ == '__main__':
             # print(f'It is not file!')  # comment for prod
             continue
         is_in_def_cfg = check_1st_cond(val1, val2, data_file_path)
-        print(f'KEY = {is_in_def_cfg}')
+        print(f'Data file in default.cfg? {is_in_def_cfg}')
         if not is_in_def_cfg:
             check_2nd_cond(cfg_dict, data_file)
